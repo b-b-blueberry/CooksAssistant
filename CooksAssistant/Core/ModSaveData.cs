f6893b72701ac86bb6fd200992789f0b157520cf
razikh-git <pocketsathome@gmail.com>
2020-10-12 02:04:45 +1100

    Completed search page with text search, filtering, favourites, correct stack flow, and results drawing. No grid view yet. Cooking page will now (incorrectly) consume dropIn ingredients for food. Some unrelated crash fixes. Rearranged spritesheet for filter icons.

--- CooksAssistant/Core/ModSaveData.cs
+++ CooksAssistant/Core/ModSaveData.cs
@@ -10,5 +10,6 @@ namespace CooksAssistant
 		public Vector2 CookingMenuButtonPosition { get; set; } = Vector2.Zero;
 		public Dictionary<string, int> FoodsEaten { get; set; } = new Dictionary<string, int>();
 		public bool IsUsingGridViewInRecipeSearch { get; set; } = false;
+		public List<string> FavouriteRecipes { get; set; } = new List<string>();
 	}
 }
